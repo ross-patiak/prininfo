35fe2505aae99c12000058411b104e77ba5ba933
Ross Patiak <ross.patiak@gmail.com>
2018-09-18 01:02:30 -0400

    problem 7 fix

--- assignment 1/problem 7.sql
+++ assignment 1/problem 7.sql
@@ -3,5 +3,5 @@
 USE BarBeerDrinker;
 
 SELECT DISTINCT t1.drinker
-FROM (SELECT drinker FROM likes WHERE beer='Bud') t1, (SELECT drinker FROM likes WHERE beer='Hefeweizen') t2
+FROM (SELECT drinker FROM likes WHERE beer='Killian''s') t1, (SELECT drinker FROM likes WHERE beer='Hefeweizen') t2
 WHERE t1.drinker=t2.drinker;
