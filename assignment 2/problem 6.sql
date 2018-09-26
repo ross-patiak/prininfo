45dd2e50f38d5d273204acc5e0d3a01023387507
Ross Patiak <ross.patiak@gmail.com>
2018-09-25 22:55:21 -0400

    problem 6 fix

--- assignment 2/problem 6.sql
+++ assignment 2/problem 6.sql
@@ -1,5 +1,6 @@
 /*Pairs of drinkers who frequent the same bar (and possibly other bars)*/
 USE BarBeerDrinker;
 
-SELECT f1.drinker, f2.drinker
+SELECT least(f1.drinker, f2.drinker) AS drinker1, greatest(f1.drinker, f2.drinker) AS drinker2
 FROM frequents f1, frequents f2
+WHERE f1.bar=f2.bar AND f1.drinker<>f2.drinker; 
