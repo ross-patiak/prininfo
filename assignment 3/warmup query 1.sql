09e87859228146fe4277e137faa5a055286ea159
Ross Patiak <ross.patiak@gmail.com>
2018-10-14 16:53:23 -0400

    hi mom

--- assignment 3/warmup query 1.sql
+++ assignment 3/warmup query 1.sql
@@ -1,1 +1,9 @@
 USE DiscoveryChallenge;
+
+/*For each restaurant, how many 3-star reviews did each get?  List these restaurants in tripadvisor rank order from top to bottom.*/
+
+SELECT RESTAURANT, RANK, COUNT(*) AS number_of_3_starred_reviews 
+FROM TRIPADVISOR 
+WHERE REVIEW_STARS = 3 
+GROUP BY RESTAURANT
+ORDER BY RANK ASC;
